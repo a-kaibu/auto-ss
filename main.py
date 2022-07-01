60553bff220da9f8bcc0f00f4b4179e75450eb17
kota <52403688+yashikota@users.noreply.github.com>
2022-07-01 23:18:40 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import win32gui
+from PIL import ImageGrab
+import time
+from tqdm import tqdm
+import os
+
+if not os.path.exists("img"):
+    os.makedirs("img")
+
+for i in tqdm(range(1,10+1)):
+    time.sleep(10)
+    x0, y0, x1, y1 = win32gui.GetWindowRect(win32gui.GetForegroundWindow())
+    im = ImageGrab.grab((x0, y0, x1, y1), all_screens=True)
+    im.save("img/" + str(i).zfill(3) + ".png")
