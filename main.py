e45b101d1e67f46e25f3e55dc7679870a889c014
kota <52403688+yashikota@users.noreply.github.com>
2022-07-23 01:26:25 +0900

    update: targetを追加

--- main.py
+++ main.py
@@ -4,11 +4,15 @@ import time
 from tqdm import tqdm
 import os
 
+target = "zombie"
+# target = "zombie2"
+# target = "creeper"
+
 if not os.path.exists("img"):
     os.makedirs("img")
 
-for i in tqdm(range(1,10+1)):
+for i in tqdm(range(1,30+1)):
     time.sleep(10)
     x0, y0, x1, y1 = win32gui.GetWindowRect(win32gui.GetForegroundWindow())
     im = ImageGrab.grab((x0, y0, x1, y1), all_screens=True)
-    im.save("img/" + str(i).zfill(3) + ".png")
+    im.save(f"img/{target}_{str(i).zfill(3)}.png")
