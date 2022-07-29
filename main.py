cdc38ec7cd32863e997b06117f62200dcba12ffd
kota <52403688+yashikota@users.noreply.github.com>
2022-07-29 09:29:32 +0900

    update: ファイル名に日付を含めるように

--- main.py
+++ main.py
@@ -3,10 +3,9 @@ from PIL import ImageGrab
 import time
 from tqdm import tqdm
 import os
+import datetime
 
-# target = "zombie"
-# target = "zombie_villager"
-target = "creeper"
+date = datetime.datetime.now().strftime("%m%d")
 
 if not os.path.exists("img"):
     os.makedirs("img")
@@ -15,4 +14,4 @@ for i in tqdm(range(1,30+1)):
     time.sleep(10)
     x0, y0, x1, y1 = win32gui.GetWindowRect(win32gui.GetForegroundWindow())
     im = ImageGrab.grab((x0, y0, x1, y1), all_screens=True)
-    im.save(f"img/{target}_{str(i).zfill(3)}.png")
+    im.save(f"img/{date}_{str(i).zfill(3)}.png")
